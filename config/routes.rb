c923c33cb9dcf33771b712b6baa41203256dc61a
andygodsey <andygodsey@gmail.com>
2015-06-18 00:21:24 -0600

    Add visitors home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
   mount Upmin::Engine => '/admin'
+  
+  authenticated do
+    root :to => 'design_applications#index', as: :authenticated
+  end
+
   root to: 'visitors#index'
+  
   devise_for :users
   resources :users
   resources :design_applications do
